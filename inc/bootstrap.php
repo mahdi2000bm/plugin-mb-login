c390249d882b12d68e90068589826b13b17c8310
Your Name <you@example.com>
2023-09-16 21:47:48 +0330

    bootstrap files

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -1,7 +1,19 @@
 <?php
 
+// Enqueue scripts for front user
+function mb_register_assets()
+{
+    //
+}
+
+// Enqueue scripts for admin dashboard
+function mb_register_assets_admin()
+{
+    //
+}
+
 // Register hooks
-add_action('wp_enqueue_scripts', 'register_assets');
-add_action('admin_enqueue_scripts', 'register_assets_admin');
+add_action('wp_enqueue_scripts', 'mb_register_assets');
+add_action('admin_enqueue_scripts', 'mb_register_assets_admin');
 
 echo "include";

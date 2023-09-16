540e7bdd3fba087e83cda07c53530f62d907d94b
Your Name <you@example.com>
2023-09-16 22:02:24 +0330

    bootstrap scripts

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -3,9 +3,9 @@
 // Enqueue scripts for front user
 function mb_register_assets()
 {
-    wp_register_style('mb-auth-global', MB_PLUGIN_URL . 'assets/css/front/style.css', '', '1.0.0');
-    wp_register_script('mb-auth-global', MB_PLUGIN_URL . 'assets/js/global.js', '["jquery"]',"", true);
-    wp_register_script('mb-auth-ajax', MB_PLUGIN_URL . 'assets/js/ajax.js', '["jquery"]',"", true);
+    wp_register_style('mb-auth-global', MB_PLUGIN_URL . 'assets/css/front/style.css', "", '1.0.0');
+    wp_register_script('mb-auth-global', MB_PLUGIN_URL . 'assets/js/global.js', "","", true);
+    wp_register_script('mb-auth-ajax', MB_PLUGIN_URL . 'assets/js/ajax.js', ["jquery"],"", true);
 
     wp_enqueue_style('mb-auth-global');
     wp_enqueue_script('mb-auth-global');

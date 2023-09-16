188d7e3544c9ea5d8e33caabca8a5991711de150
Your Name <you@example.com>
2023-09-16 22:11:27 +0330

    bootstrap auth pages

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -12,12 +12,5 @@ function mb_register_assets()
     wp_enqueue_script('mb-auth-ajax');
 }
 
-// Enqueue scripts for admin dashboard
-function mb_register_assets_admin()
-{
-    //
-}
-
 // Register hooks
 add_action('wp_enqueue_scripts', 'mb_register_assets');
-// add_action('admin_enqueue_scripts', 'mb_register_assets_admin');

f904faae8633cc3bed0e8ac28023aba714a26fe3
Your Name <you@example.com>
2023-09-17 21:48:38 +0330

    add resources

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -4,7 +4,7 @@
 function mb_register_assets()
 {
     wp_register_style('mb-auth-global', MB_PLUGIN_URL . 'assets/css/front/style.css', "", '1.0.0');
-    wp_register_script('mb-auth-global', MB_PLUGIN_URL . 'assets/js/global.js', "","", true);
+    wp_register_script('mb-auth-global', MB_PLUGIN_URL . 'assets/js/\.js', "","", true);
     wp_register_script('mb-auth-ajax', MB_PLUGIN_URL . 'assets/js/ajax.js', ["jquery"],"", true);
 
     wp_enqueue_style('mb-auth-global');
--- inc/view.php
+++ inc/view.php
@@ -1,7 +1,20 @@
-<?php function get_mb_header() { ?>
+<?php
+/**
+ * Header resources
+ *
+ * @return void
+ */
+function get_mb_header() { ?>
 <link rel="stylesheet" href="https://hub.iranserver.com/templates/seven-2021/assets/css/site.min.css">
 <?php } ?>
 
-<?php function get_mb_footer() { ?>
-<link rel="stylesheet" href="https://hub.iranserver.com/templates/seven-2021/assets/css/site.min.css">
+<?php
+/**
+ * Footer resources
+ *
+ * @return void
+ */
+function get_mb_footer() { ?>
+<script src="<?= MB_PLUGIN_URL . 'assets/js/global.js' ?>">
+<script src="<?= MB_PLUGIN_URL . 'assets/js/ajax.js' ?>">
 <?php } ?>

cb2371070adb41257e3a7b6fbdeeb8119c6489fb
Your Name <you@example.com>
2023-09-17 22:03:10 +0330

    add resources

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -3,7 +3,7 @@
 // Enqueue scripts for front user
 function mb_register_assets()
 {
-    wp_register_style('mb-auth-global', MB_PLUGIN_URL . 'assets/css/front/style.css', "", '1.0.0');
+    wp_register_style('mb-auth-global', MB_PLUGIN_URL . 'assets/css/style.css', "", '1.0.0');
     wp_register_script('mb-auth-global', MB_PLUGIN_URL . 'assets/js/global.js', "","", true);
     wp_register_script('mb-auth-ajax', MB_PLUGIN_URL . 'assets/js/ajax.js', ["jquery"],"", true);
 
--- inc/view.php
+++ inc/view.php
@@ -5,7 +5,7 @@
  * @return void
  */
 function get_mb_header() { ?>
-<link rel="stylesheet" href="<?= MB_PLUGIN_URL . 'assets/css/front/style.css' ?>">
+<link rel="stylesheet" href="<?= MB_PLUGIN_URL . 'assets/css/style.css' ?>">
 <?php } ?>
 
 <?php
@@ -15,6 +15,6 @@ function get_mb_header() { ?>
  * @return void
  */
 function get_mb_footer() { ?>
-<script src="<?= MB_PLUGIN_URL . 'assets/js/global.js' ?>" > </script>
-<script src="<?= MB_PLUGIN_URL . 'assets/js/ajax.js' ?>" > </script>
+<script src="<?= MB_PLUGIN_URL . 'assets/js/global.js' ?>"></script>
+<script src="<?= MB_PLUGIN_URL . 'assets/js/ajax.js' ?>"></script>
 <?php } ?>

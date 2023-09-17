22370c3763528685f53cae7c0a87b047a67e867c
Your Name <you@example.com>
2023-09-17 21:56:36 +0330

    add resources

--- inc/view.php
+++ inc/view.php
@@ -5,7 +5,7 @@
  * @return void
  */
 function get_mb_header() { ?>
-<link rel="stylesheet" href="https://hub.iranserver.com/templates/seven-2021/assets/css/site.min.css">
+<link rel="stylesheet" href="<?= MB_PLUGIN_URL . 'assets/css/front/style.css' ?>">
 <?php } ?>
 
 <?php
@@ -15,6 +15,6 @@ function get_mb_header() { ?>
  * @return void
  */
 function get_mb_footer() { ?>
-<script src="<?= MB_PLUGIN_URL . 'assets/js/global.js' ?>">
-<script src="<?= MB_PLUGIN_URL . 'assets/js/ajax.js' ?>">
+<script src="<?= MB_PLUGIN_URL . 'assets/js/global.js' ?>"/>
+<script src="<?= MB_PLUGIN_URL . 'assets/js/ajax.js' ?>"/>
 <?php } ?>

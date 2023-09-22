b421dbee80823c5a944e8a20cf58e4a3bdb1bf1f
Your Name <you@example.com>
2023-09-23 03:13:36 +0330

    add jquery script

--- inc/view.php
+++ inc/view.php
@@ -7,6 +7,7 @@
 function get_mb_header() { ?>
 <link rel="stylesheet" href="<?= MB_PLUGIN_URL . 'assets/css/bootstrap.css' ?>">
 <link rel="stylesheet" href="<?= MB_PLUGIN_URL . 'assets/css/style.css' ?>">
+<script src='/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.1' id='jquery-migrate-js'></script>
 <?php } ?>
 
 <?php

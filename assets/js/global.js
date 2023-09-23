a57ab9c5c2dbb67416346d03c82757c5530ab618
Your Name <you@example.com>
2023-09-23 14:51:30 +0330

    auth ajax

--- assets/js/global.js
+++ assets/js/global.js
@@ -50,7 +50,9 @@ function validateEmail (email) {
 }
 
 function response(targetId, type, message) {
-    if (type === 200) {
+    if (type === 400) {
+        $("#" + targetId).addClass('error')
+    } else if (type === 200) {
         $("#" + targetId).addClass('error')
     }
 }

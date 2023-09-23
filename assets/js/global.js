5f09f78d6aa8b64d37ba2e87d9dd75f3c52af79d
Your Name <you@example.com>
2023-09-23 14:27:41 +0330

    auth ajax

--- assets/js/global.js
+++ assets/js/global.js
@@ -10,45 +10,45 @@ jQuery(document).ready(function($)
         let mailOrPhone = $('#inputEmailPhone').val();
         authType(mailOrPhone);
     });
-
-    function validateInput (input, type, require = false) {
-
-        // returned status
-        let validate = {
-            "status" : true,
-            "message" : "",
-        };
-
-        // Input require but user dont enter
-        if (require && input === "") {
-            validate.status = false;
-            validate.message = "مقدار نمی تواند خالی باشد.";
-            return validate
-        }
-
-        // Validate input by input type
-        switch (type) {
-            case "EmailPhone":
-                    if (validateEmail(input) === null) {
-                        validate.status = false;
-                        validate.message = "لطفا ایمیل یا شماره را به صورت کامل و صحیح وارد کنید.";
-                    }
-                break;
-            case "Email":
-                validate = validateEmail(input);
-                break;
-        }
-
-        return validate;
-    }
-
-    function authType(input) {
-        console.log(input);
-    }
 })
+
+function validateInput (input, type, require = false) {
+
+    // returned status
+    let validate = {
+        "status" : true,
+        "message" : "",
+    };
+
+    // Input require but user dont enter
+    if (require && input === "") {
+        validate.status = false;
+        validate.message = "مقدار نمی تواند خالی باشد.";
+        return validate
+    }
+
+    // Validate input by input type
+    switch (type) {
+        case "EmailPhone":
+            if (validateEmail(input) === null) {
+                validate.status = false;
+                validate.message = "لطفا ایمیل یا شماره را به صورت کامل و صحیح وارد کنید.";
+            }
+            break;
+        case "Email":
+            validate = validateEmail(input);
+            break;
+    }
+
+    return validate;
+}
 
 function validateEmail (email) {
     return String(email).toLowerCase().match(
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        );
+    );
+}
+
+function authType(input) {
+    console.log(input);
 }

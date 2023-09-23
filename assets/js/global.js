e015a0ebd0a21ab839cc45650ddefd9ae8a04b81
Your Name <you@example.com>
2023-09-23 13:11:45 +0330

    auth ajax

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,11 +1,15 @@
 jQuery(document).ready(function($)
 {
-    function authType(input) {
-        console.log(input);
-    }
+    $('#inputEmailPhone').on('keyup', event => {
+        console.log(event)
+    });
 
     $('#checkLoginRegister').on('click', event => {
         let mailOrPhone = $('#inputEmailPhone').val();
         authType(mailOrPhone);
     });
+
+    function authType(input) {
+        console.log(input);
+    }
 })

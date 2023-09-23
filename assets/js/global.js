d10e95f1d2699103ae1cc40b5f6239f113a0214f
Your Name <you@example.com>
2023-09-23 14:03:36 +0330

    auth ajax

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,7 +1,9 @@
 jQuery(document).ready(function($)
 {
     $('#inputEmailPhone').on('keyup', function () {
-        console.log($(this).val())
+        let input = $(this).val();
+        let validate = validateInput(input, "EmailPhone", true);
+        console.log(validate)
     });
 
     $('#checkLoginRegister').on('click', event => {
@@ -9,7 +11,33 @@ jQuery(document).ready(function($)
         authType(mailOrPhone);
     });
 
+    function validateInput (input, type, require = false) {
+
+        if (require) {
+            if (input === "")
+                return "خالی است";
+        }
+
+        switch (type) {
+            case "EmailPhone":
+                    return validateEmail(input);
+                break;
+            case "":
+                break;
+            case "":
+                break;
+        }
+    }
+
     function authType(input) {
         console.log(input);
     }
 })
+
+function validateEmail (email) {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+}

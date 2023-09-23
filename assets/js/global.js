fcfc710d2f5f743e9a2de07080be3941d56bde4b
Your Name <you@example.com>
2023-09-23 14:05:27 +0330

    auth ajax

--- assets/js/global.js
+++ assets/js/global.js
@@ -35,9 +35,7 @@ jQuery(document).ready(function($)
 })
 
 function validateEmail (email) {
-    return String(email)
-        .toLowerCase()
-        .match(
+    return String(email).toLowerCase().match(
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         );
 }

39a4153fe1dde0393e3b79baa93e7206a591beb6
Your Name <you@example.com>
2023-09-24 13:21:05 +0330

    auth ajax

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,11 +1,11 @@
-jQuery(document).ready(function($)
-{
+jQuery(document).ready(function($) {
+    // When user enter phone or email address
     $('#inputEmailPhone').on('keyup', function () {
         let input = $(this).val();
         let validate = validateInput(input, "EmailPhone", true);
         responseView("inputEmailPhone", validate.status, validate.message);
     });
-
+    // When user click on continue
     $('#checkLoginRegister').on('click', event => {
         let mailOrPhone = $('#inputEmailPhone').val();
         let validate = validateInput(mailOrPhone, "EmailPhone", true);
@@ -17,6 +17,14 @@ jQuery(document).ready(function($)
     });
 })
 
+/**
+ * Validates the input and returns the response in json format
+ *
+ * @param input
+ * @param type
+ * @param require
+ * @returns {{message: string, status: number}}
+ */
 function validateInput (input, type, require = false) {
 
     // returned status
@@ -48,18 +56,37 @@ function validateInput (input, type, require = false) {
     return validate;
 }
 
+/**
+ * if not valid null returned
+ *
+ * @param mobile
+ * @returns {RegExpMatchArray}
+ */
 function validateEmail (email) {
     return String(email).toLowerCase().match(
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
 }
 
+/**
+ * if not valid null returned
+ *
+ * @param mobile
+ * @returns {RegExpMatchArray}
+ */
 function validatePhone (mobile) {
     return String(mobile).toLowerCase().match(
         /((0?9)|(\+?989))\d{9}/g
     );
 }
 
+/**
+ * Generated error to selected field
+ *
+ * @param targetId
+ * @param type
+ * @param message
+ */
 function responseView(targetId, type, message) {
     if (type === 400) {
         jQuery("#" + targetId).addClass('valid-error');
@@ -71,6 +98,12 @@ function responseView(targetId, type, message) {
     }
 }
 
+/**
+ * Specifying the input type and calling the corresponding Ajax
+ * Must be email or phone number
+ *
+ * @param input
+ */
 function authType(input) {
     console.log(input);
 }

afecd2d1f0f6c9cc800d5338e0ff5a2617252a7c
Your Name <you@example.com>
2023-09-24 16:05:38 +0330

    auth ajax

--- inc/controller.php
+++ inc/controller.php
@@ -8,5 +8,5 @@ defined("ABSPATH") || exit();
 add_action('wp_ajax_nopriv_mb_login_via_email', "mb_login_via_email");
 
 function mb_login_via_email() {
-	wp_send_json('[200 => "ok"]', 200);
+	wp_send_json(json_encode([200 => "ok"]), 200);
 }

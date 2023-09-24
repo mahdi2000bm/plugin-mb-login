c24efc39084ee70b41a5f760fbfd8b9150e99a2f
Your Name <you@example.com>
2023-09-24 14:49:42 +0330

    auth ajax

--- inc/view.php
+++ inc/view.php
@@ -19,12 +19,11 @@ function get_mb_header() { ?>
  */
 function get_mb_footer() {  ?>
 
-<?php
-$mb_ajax = [
+<?php $mb_ajax = [
+    // Localizes ajax and nonce
     'ajaxurl' => admin_url('admin-ajax.php'),
-    '_nonce' => wp_create_nonce()
-]  ?>
-<script><?php echo  json_encode($mb_ajax)?></script>
+    '_nonce' => wp_create_nonce() ] ?>
+<script><?= json_encode($mb_ajax) ?></script>
 <script src="<?= MB_PLUGIN_URL . 'assets/js/ajax.js' ?>"></script>
 <script id="global-auth" src="<?= MB_PLUGIN_URL . 'assets/js/global.js' ?>"></script>
 <?php } ?>

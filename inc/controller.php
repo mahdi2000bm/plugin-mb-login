6f4e22e92807b698cd66b0e282c85ef13051574c
Your Name <you@example.com>
2023-09-24 17:00:35 +0330

    redirect login user

--- inc/controller.php
+++ inc/controller.php
@@ -10,9 +10,9 @@ add_action('wp_ajax_nopriv_mb_login_via_phone', "mb_login_via_phone");
 
 function mb_login_via_email() {
 	$email = $_POST['input'];
-	wp_send_json(json_encode([200 => "email"]), 200);
+	wp_send_json($email, 200);
 }
 function mb_login_via_phone() {
 	$phone = $_POST['phone'];
-	wp_send_json(json_encode([200 => "phone"]), 200);
+	wp_send_json($phone, 200);
 }
